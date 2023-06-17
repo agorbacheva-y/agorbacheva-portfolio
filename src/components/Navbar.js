580b8dfce7a9a4fa87db21e274a501c183430a6a
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-17 14:23:30 +0200

    icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,30 @@
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Button from "react-bootstrap/Button";
+import { MdMenu } from "react-icons/md";
 
 const Navbar = () => {
   return (
     <div>
+      <Button
+        className="d-none"
+      >
+        <MdMenu />
+      </Button>
+
+      <p>desktop and tablet</p>
+
+      <Offcanvas
+        responsive="xs"
+      >
+        <Offcanvas.Header closeButton></Offcanvas.Header>
+        <Offcanvas.Body>
+          <ul className="mb-0">
+            <li>link 1</li>
+            <li>link 2</li>
+            <li>link 3</li>
+          </ul>
+        </Offcanvas.Body>      
+      </Offcanvas>
     </div>
   );
 };
