0948a493785b57ebacef89f8ceb9ffb087f503d1
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-18 23:17:10 +0200

    Skills.js - array of icons

---
+++ src/components/Skills.js
@@ -0,0 +1,22 @@
+import { FaHtml5, FaCss3Alt, FaSass, SiJavascript, FaReact, FaFigma, FaWindows } from "react-icons";
+
+const Skills = () => {
+
+  const skills = [
+    FaHtml5,
+    FaCss3Alt,
+    FaSass,
+    SiJavascript,
+    FaReact,
+    FaFigma,
+    FaWindows
+  ];
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Skills;
