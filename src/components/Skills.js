fccbfaed6e1715b3bb3f4cc01e109a21cfad286b
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-19 23:07:47 +0200

    add label to icons in Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,43 +1,38 @@
 import { FaHtml5, FaCss3Alt, FaSass, FaReact, FaFigma, FaWindows } from "react-icons/fa";
+import Icon from '@mui/material/Icon';
 import { SiJavascript } from "react-icons/si";
 import Title from "./Title";
 
 const Skills = () => {
 
-  const skills = [
-    FaHtml5,
-    FaCss3Alt,
-    FaSass,
-    SiJavascript,
-    FaReact,
-    FaFigma,
-    FaWindows
+  const icons = [
+    {icon: FaHtml5, name: "HTML"},
+    {icon: FaCss3Alt, name: "CSS"},
+    {icon: FaSass, name: "Sass"},
+    {icon: SiJavascript, name: "JavaScript"},
+    {icon: FaReact, name: "React"},
+    {icon: FaFigma, name: "Figma"},
+    {icon: FaWindows, name: "Windows"}
   ];
 
    return (
     <div className="skills">
       <Title>Skills</Title>
 
-      <div className="skills-container">
-        {skills.map((item, i) => {
-          const MySkills = skills[i]
-          return (
-            <ul key={item} className="skills-list">
+      {icons.map(arr => {
+        return (
+          <div className="skills-container">
+            <ul className="skills-list">
               <li>
-                <MySkills />
+                <Icon component={arr.icon} />
+                {arr.name}
               </li>
             </ul>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
 
 export default Skills;
-
-{/* <div className="skills-icon">
-{skills.map((icon, i) => (
-  <p key={icon}>{icon}</p>
-))}
-</div> */}
