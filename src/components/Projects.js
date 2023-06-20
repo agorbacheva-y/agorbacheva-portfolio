3affdfb15db079fdb3a28a5100cf94899aef3747
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-20 23:20:32 +0200

    add project text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,20 @@ const Projects = () => {
         <div className="project__overlay">
           <div className="project__text">
             <h5>SmartKids Learning Game</h5>
+            <p>A React game for young children to develop vocabulary
+              and shape recognition. Developed as a group project during 
+              Sundsgårdens IT course.
+            </p>
+            <div>
+              <button>React</button>
+              <button>React Router</button>
+              <button>Web Speech API</button>
+            </div>
+            <div>
+              <button>View live</button>
+              <button>See code</button>
+            </div>
+            
           </div>
         </div>  
       </div>
