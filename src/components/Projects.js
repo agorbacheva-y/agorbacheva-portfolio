6ff33ec4abd2a5932d8c712128d66da8b0b3434d
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-20 12:44:10 +0200

    project text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,11 @@ const Projects = () => {
       <Title>Recent Work</Title>
       <div className="project__container">
         <img src={mainmenu} alt="main menu" className="project__img" />
-        <div className="project__overlay"></div>  
+        <div className="project__overlay">
+          <div className="project__text">
+            <h5>SmartKids Learning Game</h5>
+          </div>
+        </div>  
       </div>
     </div>
   );
