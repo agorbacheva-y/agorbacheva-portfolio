42ea5d518050a29a530c88b2f6c6c618a30c6350
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-20 12:40:15 +0200

    project screenshot and overlay

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,14 @@
 import Title from "./Title";
+import mainmenu from "../images/mainmenu.png"
 
 const Projects = () => {
   return (
     <div className="projects">
       <Title>Recent Work</Title>
+      <div className="project__container">
+        <img src={mainmenu} alt="main menu" className="project__img" />
+        <div className="project__overlay"></div>  
+      </div>
     </div>
   );
 };
