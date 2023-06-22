f120717c12ee8e73b8722f0017d14458a096a9ec
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-22 11:33:10 +0200

    add herotofu for form backend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Title from "./Title";
 import CustomTextField from "./CustomTextField";
 
+const FORM_ENDPOINT = "https://public.herotofu.com/v1/c27aab20-10dd-11ee-a6b0-17653bd30bd3";
+
 const Contact = () => {
   // store input
   const [ inputValue, setInputValue ] = useState({
@@ -10,16 +12,38 @@ const Contact = () => {
     message: ""
   });
 
+  const [ submitted, setSubmitted ] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setInputValue((prevInputValue) => ({ ...prevInputValue, [name]: value }));
+
+    console.log(inputValue.fullName, inputValue.email, inputValue.message);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(inputValue.fullName, inputValue.email, inputValue.message)
+    const data = {};
 
+    fetch(FORM_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Form response was not ok');
+        }
+        setSubmitted(true);
+      })
+      .catch((err) => {
+        e.target.submit();
+      });
+  
     clearInput();
     setInputValue({ fullName: "", email: "", message: "" });
   };
@@ -28,12 +52,21 @@ const Contact = () => {
     setInputValue({ fullName: "", email: "", message: "" })
   };
 
+  if (submitted) {
+    return (
+      <>
+        <p>Thank you!</p>
+        <p>We'll be in touch soon.</p>
+      </>
+    );
+  }
+
   return (
     <div className="contact">
       <Title>Contact</Title>
 
       <div>
-        <form className="form" onSubmit={handleSubmit}>
+        <form className="form" onSubmit={handleSubmit} method="POST" action={FORM_ENDPOINT} >
           <CustomTextField 
             type="text"
             label="Name" 
