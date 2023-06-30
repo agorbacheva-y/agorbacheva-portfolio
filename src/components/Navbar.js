88c6546b73eb0a1222c8ee210febcc2c9357d170
agorbacheva-y <agorbacheva.y@gmail.com>
2023-06-30 13:26:49 +0200

    state to show navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,37 @@
 import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
-  const [ showNavbar, setShowNavbar ]
+  const [ showNavbar, setShowNavbar ] = useState(false);
+
+  const handleShow = () => {
+    setShowNavbar(!showNavbar);
+  };
 
   return (
     <nav>
       <div className='navbar__container'>
-        <ul>
-          <li>
-            <NavLink className="navbar__link" to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink className="navbar__link" to="/aboutme">About</NavLink>
-          </li>
-          <li>
-            <NavLink className="navbar__link" to="/projects">Projects</NavLink>
-          </li>
-          <li>
-            <NavLink className="navbar__link" to="/contact">Contact</NavLink>
-          </li>
-        </ul>
+        <div className='navbar__menu'>
+          <MenuIcon onClick={handleShow} />
+        </div>
+
+        <div className={`navbar__links ${showNavbar && 'active'}`} >
+          <ul>
+            <li>
+              <NavLink className="navbar__link" to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink className="navbar__link" to="/aboutme">About</NavLink>
+            </li>
+            <li>
+              <NavLink className="navbar__link" to="/projects">Projects</NavLink>
+            </li>
+            <li>
+              <NavLink className="navbar__link" to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
