22032ec08604ba6912aa16e0c97cf539f5a1a57e
agorbacheva-y <agorbacheva.y@gmail.com>
2023-07-01 16:20:11 +0200

    set state to navbar menu and close icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,56 @@
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
   const [ showNavbar, setShowNavbar ] = useState(false);
 
   const handleShow = () => {
-    setShowNavbar(!showNavbar);
+    setShowNavbar(true);
   };
 
-  return (
-    <nav>
-      <div className='navbar__container'>
-        <div className='navbar__menu'>
-          <MenuIcon onClick={handleShow} />
-        </div>
+  const handleClose = () => {
+    setShowNavbar(false);
+  }
 
-        <div className={`navbar__links ${showNavbar && 'active'}`} >
-          <ul>
-            <li>
-              <NavLink className="navbar__link" to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink className="navbar__link" to="/aboutme">About</NavLink>
-            </li>
-            <li>
-              <NavLink className="navbar__link" to="/projects">Projects</NavLink>
-            </li>
-            <li>
-              <NavLink className="navbar__link" to="/contact">Contact</NavLink>
-            </li>
-          </ul>
+  return (
+    <nav className='navbar'>
+
+        <button 
+          className="navbar__menu" 
+          onClick={handleShow}
+        >
+          <MenuIcon  />
+        </button>
+        
+      
+
+        <div className="navbar__elements">
+          { showNavbar && (
+            <>
+              <ul>
+                <li>
+                  <NavLink className="navbar__link" to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink className="navbar__link" to="/aboutme">About</NavLink>
+                </li>
+                <li>
+                  <NavLink className="navbar__link" to="/projects">Projects</NavLink>
+                </li>
+                <li>
+                  <NavLink className="navbar__link" to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+          
+              <button className="navbar__close">
+                <CloseIcon onClick={handleClose} />
+              </button>
+            </>
+          )}
         </div>
-      </div>
+        
     </nav>
   );
 };
