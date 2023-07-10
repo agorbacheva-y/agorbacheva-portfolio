624cda5ceaefdd3e5e68cb4ce3d2e1e5fc850de4
agorbacheva-y <agorbacheva.y@gmail.com>
2023-07-10 23:29:49 +0200

    add bhromaon project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@ import { forwardRef } from "react";
 import Title from "./Title";
 import login from "../images/login.png";
 import trivia from "../images/trivia.png";
+import css from "../images/bhromaon.png";
 import Stack from "@mui/material/Stack";
 import CustomButton from "./CustomButton";
 
@@ -20,8 +21,9 @@ const Projects = ( props, ref ) => {
             <div className="project__title">
               <h4>SmartKids Learning Game</h4>
               <p>A game created in React for young children to develop vocabulary
-                and shape recognition. Developed as a group project during 
-                Sundsgårdens IT course.
+                and shape recognition. Created as a group project in 
+                Sundsgårdens IT course. Features worked on: Register new user 
+                (removed on live site), Body part game, Face part game
               </p>
             </div>
             <div className="project__tech__container">
@@ -45,6 +47,38 @@ const Projects = ( props, ref ) => {
         </div>  
       </div>
 
+      {/* html css final proj */}
+      <div className="project__container">
+        <img src={css} alt="main menu" className="project__img" />
+        
+        <div className="project__overlay">
+          <div className="project__overlay__text">
+            <div className="project__title">
+              <h4>HTML/CSS Individual Project</h4>
+              <p>The final individual project in Sundsgårdens IT course using html, css, scss
+                for a responsive web design.
+              </p>
+            </div>
+            <div className="project__tech__container">
+              <div className="project__tech__button">HTML/CSS</div>
+              <div className="project__tech__button">Sass</div>
+            </div>
+            <Stack 
+              spacing={3} 
+              direction="row" 
+              justifyContent="center"
+            >
+              <a href="https://agorbacheva-tfwsundsgarden-css-proj.netlify.app/" target="blank">
+                <CustomButton>Live site</CustomButton>
+              </a>
+              <a href="https://github.com/agorbacheva-y/Bhromaon.git" target="blank">
+                <CustomButton>Github</CustomButton>
+              </a>
+            </Stack>
+          </div>
+        </div>  
+      </div>
+
       {/* Trivia API Game */}
       <div className="project__container">
         <img src={trivia} alt="main menu" className="project__img" />
@@ -52,8 +86,9 @@ const Projects = ( props, ref ) => {
         <div className="project__overlay">
           <div className="project__overlay__text">
             <div className="project__title">
-              <h4>Trivia Game</h4>
+              <h4>Trivia Game (in progress)</h4>
               <p>A trivia game in React using the Open Trivia DB API.
+                In progress and hopefully live soon!
               </p>
             </div>
             <div className="project__tech__container">
